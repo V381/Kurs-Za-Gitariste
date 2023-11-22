01d8b38d944e4d3fe6ff18524e57d490ee95fd32
Pavle Paunovic <paavlee@yahoo.com>
2023-11-22 10:28:52 +0100

    Update 1

--- javascript/main.mjs
+++ javascript/main.mjs
@@ -1,4 +1,9 @@
 import { scrollFromHeader } from "./scroll-from-header/_scroll-from-header.mjs";
 
-
-scrollFromHeader();
+(() => {
+    scrollFromHeader(".scroll-target", '.scroll-down');
+    scrollFromHeader(".learning-path", ".scroll-down-learning-path")
+    scrollFromHeader(".web-application-section" , ".scroll-down-web-app");
+    scrollFromHeader(".scroll-to-path-second", ".scroll-down-to-content");
+    scrollFromHeader(".web-app-scroll", ".scroll-down-to-youtube");
+})();
--- javascript/scroll-from-header/_scroll-from-header.mjs
+++ javascript/scroll-from-header/_scroll-from-header.mjs
@@ -1,12 +1,12 @@
-export function scrollFromHeader() {
+export function scrollFromHeader(target, scrollIconClass) {
     function scrollToTarget() {
-        const targetElement = document.querySelector('.scroll-target');
+        const targetElement = document.querySelector(target);
         if (targetElement) {
             targetElement.scrollIntoView({ behavior: 'smooth' });
         }
     }
 
-    const scrollDownIcon = document.querySelector('.scroll-down');
+    const scrollDownIcon = document.querySelector(scrollIconClass);
     if (scrollDownIcon) {
         scrollDownIcon.addEventListener('click', scrollToTarget);
     }
