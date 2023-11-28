c5a4d0af072962870e38b49e01f62fb2d182a940
Pavle Paunovic <paavlee@yahoo.com>
2023-11-28 12:48:26 +0100

    Add new page

--- javascript/main.mjs
+++ javascript/main.mjs
@@ -8,3 +8,18 @@ import { scrollFromHeader } from "./scroll-from-header/_scroll-from-header.mjs";
     scrollFromHeader(".web-app-scroll", ".scroll-down-to-youtube");
     scrollFromHeader(".book-of-shred", ".scroll-down-to-book-of-shred");
 })();
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
